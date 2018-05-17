9efa1a5955f56ee36bade2d7f548a72bbce4ede2
関口 司 <t14159265358979323846@gmail.com>
2018-05-17 14:03:53 +0900

    ナビゲーションバーの社員管理をログインしている場合のみ表示しているようにした

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,7 +34,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav mr-auto">
-                        <li>社員管理</li>
+                        @auth
+                            <li>社員管理</li>
+                        @endauth
                     </ul>
 
                     <!-- Right Side Of Navbar -->
