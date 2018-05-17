033e984870ce5bd8ae95e6ffcedee4e1e898da6f
関口 司 <t14159265358979323846@gmail.com>
2018-05-17 15:07:46 +0900

    新規社員登録画面のレイアウトを整備した

--- resources/views/employees/edit.blade.php
+++ resources/views/employees/edit.blade.php
@@ -8,13 +8,17 @@
                     <div class="card-header">新規社員登録</div>
 
                     <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success">
-                                {{ session('status') }}
+                        <form>
+                            <div class="form-group">
+                                <label for="employeeNo">社員番号</label>
+                                <input type="email" class="form-control" id="employeeNo" placeholder="123456789">
                             </div>
-                        @endif
-
-                        You are logged in!
+                            <div class="form-group">
+                                <label for="name">氏名</label>
+                                <input type="password" class="form-control" id="name" placeholder="山田花子">
+                            </div>
+                            <button type="submit" class="btn btn-primary">登録</button>
+                        </form>
                     </div>
                 </div>
             </div>
