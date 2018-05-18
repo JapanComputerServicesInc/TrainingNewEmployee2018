c0226cd1d7941781309f47894709b5ba4fbfa05c
関口 司 <t14159265358979323846@gmail.com>
2018-05-18 18:57:04 +0900

    微修正

--- resources/views/employees/_delete_model.blade.php
+++ resources/views/employees/_delete_model.blade.php
@@ -1,8 +1,8 @@
-<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel" aria-hidden="true">
     <div class="modal-dialog" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel">対象の社員を削除します</h5>
+                <h5 class="modal-title" id="deleteModalLabel">対象の社員を削除します</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
